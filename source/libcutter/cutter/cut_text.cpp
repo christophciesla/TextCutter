3b7f718942b38ea4d737d90d5b9b4198adf522dc
Christoph Ciesla <christoph.ciesla@mytum.de>
2022-03-18 00:08:38 +0100

    Split project into lib for logic and gui
    Issue: #5

---
+++ source/libcutter/cutter/cut_text.cpp
@@ -0,0 +1,29 @@
+#include "cutter/cut_text.h"
+
+namespace cutter
+{
+QVector<QString> CutText(const QString& text, const std::int32_t num_parts)
+{
+    QVector<QString> parts{ num_parts };
+
+    const std::int32_t part_length{ (text.length() + num_parts - 1) / 4 };
+    for (QString& part : parts)
+    {
+        part.reserve(part_length);
+    }
+
+    for (std::int32_t i{ 0 }; i < text.length(); ++i)
+    {
+        const QChar c{ text[i] };
+        if ((c == ' ') || (c == '\n'))
+        {
+            parts[i % num_parts].append('_');
+        }
+        else
+        {
+            parts[i % num_parts].append(c);
+        }
+    }
+    return parts;
+}
+}
