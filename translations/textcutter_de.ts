bcff12ddd6db1b63283af4e1cc77f44055ec3f4c
Christoph Ciesla <christoph.ciesla@mytum.de>
2022-03-25 20:54:25 +0100

    Support input file in cmdline app
    Issue: #2

--- translations/textcutter_de.ts
+++ translations/textcutter_de.ts
@@ -4,64 +4,80 @@
 <context>
     <name>QCoreApplication</name>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="19"/>
+        <location filename="../source/textcutter/main.cpp" line="20"/>
         <source>Invalid format.</source>
         <translation>Ungültiges format.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="20"/>
-        <location filename="../source/textcutter/main.cpp" line="25"/>
+        <location filename="../source/textcutter/main.cpp" line="21"/>
+        <location filename="../source/textcutter/main.cpp" line="26"/>
         <source>Creating %1 text parts which is minimum.</source>
         <translation>Erstelle %1 Textteile als Minimum.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="30"/>
+        <location filename="../source/textcutter/main.cpp" line="31"/>
         <source>Creating %1 text parts which is maximum.</source>
         <translation>Erstelle %1 Textteile als Maximum.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="40"/>
+        <location filename="../source/textcutter/main.cpp" line="41"/>
         <source>Part %1:</source>
         <translation>Teil %1:</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="64"/>
+        <location filename="../source/textcutter/main.cpp" line="66"/>
         <source>Text cutter</source>
         <translation>Textzerhacker</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="67"/>
+        <location filename="../source/textcutter/main.cpp" line="72"/>
         <source>Cuts a text char by char into up to 10 parts.</source>
         <translation>Zerhackt einen Text Zeichen für Zeichen in bis zu 10 Teile.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="72"/>
+        <location filename="../source/textcutter/main.cpp" line="77"/>
         <source>Number of parts to be generated.</source>
         <translation>Anzahl der zu erzeugenden Teile.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="73"/>
+        <location filename="../source/textcutter/main.cpp" line="78"/>
         <source>If not specified, the text will be cut into two parts.</source>
         <translation>Falls nicht angegeben, wird der Text in zwei Teile geteilt.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="74"/>
+        <location filename="../source/textcutter/main.cpp" line="79"/>
         <source>number</source>
         <translation>Anzahl</translation>
     </message>
     <message>
+        <location filename="../source/textcutter/main.cpp" line="96"/>
+        <source>file name</source>
+        <translation>Dateiname</translation>
+    </message>
+    <message>
         <location filename="../source/textcutter/main.cpp" line="95"/>
+        <location filename="../source/textcutter/main.cpp" line="99"/>
         <source>text</source>
         <translation>Text</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="96"/>
+        <location filename="../source/textcutter/main.cpp" line="93"/>
+        <source>Path to file with the original text.</source>
+        <translation>Pfad zur Datei mit dem Originaltext.</translation>
+    </message>
+    <message>
+        <location filename="../source/textcutter/main.cpp" line="94"/>
+        <source>If specified, the &quot;%1&quot; command line argument will be ignored.</source>
+        <translation>Falls angegeben, wird das Kommandozeilenargument &quot;%1&quot; ignoriert.</translation>
+    </message>
+    <message>
+        <location filename="../source/textcutter/main.cpp" line="100"/>
         <source>The original text to be cut.</source>
         <translation>Der zu zerhackende Originaltext.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="105"/>
-        <location filename="../source/textcutter/main.cpp" line="111"/>
+        <location filename="../source/textcutter/main.cpp" line="114"/>
+        <location filename="../source/textcutter/main.cpp" line="132"/>
         <source>Input text is empty.</source>
         <translation>Eingabe-Text ist leer.</translation>
     </message>
--- translations/textcutter_en.ts
+++ translations/textcutter_en.ts
@@ -4,64 +4,80 @@
 <context>
     <name>QCoreApplication</name>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="19"/>
+        <location filename="../source/textcutter/main.cpp" line="20"/>
         <source>Invalid format.</source>
         <translation>Invalid format.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="20"/>
-        <location filename="../source/textcutter/main.cpp" line="25"/>
+        <location filename="../source/textcutter/main.cpp" line="21"/>
+        <location filename="../source/textcutter/main.cpp" line="26"/>
         <source>Creating %1 text parts which is minimum.</source>
         <translation>Creating %1 text parts which is minimum.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="30"/>
+        <location filename="../source/textcutter/main.cpp" line="31"/>
         <source>Creating %1 text parts which is maximum.</source>
         <translation>Creating %1 text parts which is maximum.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="40"/>
+        <location filename="../source/textcutter/main.cpp" line="41"/>
         <source>Part %1:</source>
         <translation>Part %1:</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="64"/>
+        <location filename="../source/textcutter/main.cpp" line="66"/>
         <source>Text cutter</source>
         <translation>Text cutter</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="67"/>
+        <location filename="../source/textcutter/main.cpp" line="72"/>
         <source>Cuts a text char by char into up to 10 parts.</source>
         <translation>Cuts a text char by char into up to 10 parts.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="72"/>
+        <location filename="../source/textcutter/main.cpp" line="77"/>
         <source>Number of parts to be generated.</source>
         <translation>Number of parts to be generated.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="73"/>
+        <location filename="../source/textcutter/main.cpp" line="78"/>
         <source>If not specified, the text will be cut into two parts.</source>
         <translation>If not specified, the text will be cut into two parts.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="74"/>
+        <location filename="../source/textcutter/main.cpp" line="79"/>
         <source>number</source>
         <translation>number</translation>
     </message>
     <message>
+        <location filename="../source/textcutter/main.cpp" line="96"/>
+        <source>file name</source>
+        <translation>file name</translation>
+    </message>
+    <message>
         <location filename="../source/textcutter/main.cpp" line="95"/>
+        <location filename="../source/textcutter/main.cpp" line="99"/>
         <source>text</source>
         <translation>text</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="96"/>
+        <location filename="../source/textcutter/main.cpp" line="93"/>
+        <source>Path to file with the original text.</source>
+        <translation>Path to file with the original text.</translation>
+    </message>
+    <message>
+        <location filename="../source/textcutter/main.cpp" line="94"/>
+        <source>If specified, the &quot;%1&quot; command line argument will be ignored.</source>
+        <translation>If specified, the &quot;%1&quot; command line argument will be ignored.</translation>
+    </message>
+    <message>
+        <location filename="../source/textcutter/main.cpp" line="100"/>
         <source>The original text to be cut.</source>
         <translation>The original text to be cut.</translation>
     </message>
     <message>
-        <location filename="../source/textcutter/main.cpp" line="105"/>
-        <location filename="../source/textcutter/main.cpp" line="111"/>
+        <location filename="../source/textcutter/main.cpp" line="114"/>
+        <location filename="../source/textcutter/main.cpp" line="132"/>
         <source>Input text is empty.</source>
         <translation>Input text is empty.</translation>
     </message>
