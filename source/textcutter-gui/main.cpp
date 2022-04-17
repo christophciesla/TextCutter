9df85671035b5ba869fbd063fcf0597e05058f9b
Christoph Ciesla <christoph.ciesla@mytum.de>
2022-04-17 14:16:01 +0200

    Add qt translation to gui app

--- source/textcutter-gui/main.cpp
+++ source/textcutter-gui/main.cpp
@@ -6,11 +6,19 @@ int main( int argc, char* argv[] )
 {
     QApplication app{ argc, argv };
 
-    QTranslator translator{};
-    const bool success{translator.load(QLocale(), "textcutter-gui", "_", ":/translations")};
+    QTranslator qt_translator{};
+    bool success{ qt_translator.load(QLocale(), "qtbase", "_",
+        QLibraryInfo::location(QLibraryInfo::TranslationsPath)) };
     if (success)
     {
-        std::ignore = app.installTranslator(&translator);
+        std::ignore = app.installTranslator(&qt_translator);
+    }
+
+    QTranslator app_translator{};
+    success = app_translator.load(QLocale(), "textcutter-gui", "_", ":/translations");
+    if (success)
+    {
+        std::ignore = app.installTranslator(&app_translator);
     }
 
     app.setApplicationName(QApplication::tr("Text cutter"));
